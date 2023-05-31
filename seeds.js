2e1118c7984287076ad400436f9718e543ba92dd
Jackson Yin <Yrh7383111@outlook.com>
2023-05-31 14:31:31 +0800

    Clean up seeds.js

--- seeds.js
+++ seeds.js
@@ -24,19 +24,16 @@ let seeds = [
 
 
 async function seedDB() {
-    try
-    {
+    try {
         await Campground.remove({});
         console.log('Campgrounds removed');
         await Comment.remove({});
         console.log('Comments removed');
 
-        for(const seed of seeds)
-        {
+        for (const seed of seeds) {
             let campground = await Campground.create(seed);
             console.log('Campground created');
-            let comment = await Comment.create(
-                {
+            let comment = await Comment.create({
                     text: 'This place is great, but I wish there was internet',
                     author: 'Homer'
                 }
@@ -47,8 +44,7 @@ async function seedDB() {
             console.log('Comment added to campground');
         }
     }
-    catch(err)
-    {
+    catch(err) {
         console.log(err);
     }
 }
